ecffb498d0a93150175ae49d060c1e05cc8a33b4
batlukov <mence47@yandex.ru>
2019-07-05 13:25:01 +0300

    added database connection and ready for tests

--- person/src/main/java/servlets/MyContextServletListener.java
+++ person/src/main/java/servlets/MyContextServletListener.java
@@ -4,13 +4,37 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
+import java.sql.*;
+
 public class MyContextServletListener implements ServletContextListener {
+
+    private Connection conn;
 
     public void contextInitialized(ServletContextEvent sce) {
         System.out.println("Application has started!");
+        try {
+            Class.forName("org.h2.Driver"); 
+            conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "sa");
+            ServletContext sc = sce.getServletContext();
+            sc.setAttribute("connect", conn);
+        } catch (SQLException e) {
+        } catch (ClassNotFoundException e) {}
+        
+        if (conn != null)
+            System.out.println("Connection to data base established!");
+        else
+            System.out.println("Cant establish connection to database");
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
         System.out.println("Application has stoped working!");
+
+        if (conn != null) {
+            try {
+                conn.close();
+                System.out.println("Connection to data base terminated!");
+            } catch (SQLException e) {}
+        }
+            
     }
 }
--- person/src/main/webapp/result.jsp
+++ person/src/main/webapp/result.jsp
@@ -1,12 +1,25 @@
 <%@ page contentType = "text/html;charset=utf-8" %> <!--- response.setContentType("charset="utf-8""); --->
+<%@ page import = "java.sql.*" %>
+<%@ page import = "javax.servlet.*" %>
 <html>
     <head>
         <title>Result page</title>
     </head>
     <body>
-        <p>Hello <%= (String)session.getAttribute("lastname")%> 
-            <%= (String)session.getAttribute("name")%> 
-            <%= request.getParameter("thirdname")%> </p>
+        <%  int id = 1; 
+            String first = (String)session.getAttribute("name"); 
+            String last = (String)session.getAttribute("lastname"); 
+            String middle = request.getParameter("thirdname");
+            ServletContext sc = session.getServletContext();
+            Connection conn = (Connection)sc.getAttribute("connect");
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO PEOPLE ("+id+","+first+","+last+","+middle+");";
+            //ResultSet rs = stmt.executeQuery(sql);
+            out.println(sql);
+         %>
+        <p>Hello <%= last%> 
+            <%= first %> 
+            <%= middle %> </p>
             <% session.invalidate(); %>
     </body>
 </html>
